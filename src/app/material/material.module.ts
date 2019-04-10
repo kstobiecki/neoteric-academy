5f8514c9945cc631bf6cce5072627e2061f72b79
kamil stobiecki <kamil.stobiecki@neoteric.eu>
2019-04-10 11:09:43 +0200

    chore(): fix mat slider import in material.module

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,17 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatSlider } from '@angular/material';
+import { MatSliderModule } from '@angular/material';
 
 
 // in this module we import every angular material module
 @NgModule({
   imports: [
     CommonModule,
-    MatSlider
+    MatSliderModule
   ],
   exports: [
     CommonModule,
-    MatSlider
+    MatSliderModule
   ]
 })
 export class MaterialModule { }
